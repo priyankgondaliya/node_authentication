d0da39e47bd309b40b344e81cbdcdd7d23fae9d1
priyank <priyank.gondaliya@phpdots.com>
2024-06-19 12:01:00 +0530

    first commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/mongoJoinsDemo", {
+mongoose.connect(process.env.DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
